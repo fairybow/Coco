6dd7c82559e1c38fb73a03592f39cf1a46e4457d
jackson <63016612+fairybow@users.noreply.github.com>
2025-05-21 15:03:52 -0400

    Adding single instance helper class

---
+++ Coco/include/Coco/StartCop.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#if defined(QT_NETWORK_LIB) || defined(QT_QTNETWORK_MODULE_H)
+
+#include <QLocalServer>
+#include <QLocalSocket>
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QTimer>
+
+namespace Coco
+{
+    /// @brief Coco::StartCop is a QObject-derived utility designed to manage
+    /// single-instance applications by detecting if another instance is running
+    /// and stopping it after sending the second instance's command line
+    /// arguments to the primary instance.
+    ///
+    /// @example
+    ///     #include "Coco/StartCop.h"
+    ///
+    ///     int main(int argc, char* argv[])
+    ///     {
+    ///         Coco::StartCop s("My App", argc, argv);
+    ///         if (s.isRunning()) return 0;
+    ///
+    ///         QApplication app(argc, argv);
+    ///
+    ///         MainWindow w;
+    ///         app.connect(&s, &StartCop::appRelaunched,
+    ///             &w, &MainWindow::slot);
+    ///         w.show();
+    ///
+    ///         return app.exec();
+    ///     }
+    class StartCop : public QObject
+    {
+        Q_OBJECT
+
+    public:
+        StartCop
+        (
+            const QString& key,
+            const int& argc = 0,
+            const char* const* argv = nullptr
+        );
+
+        virtual ~StartCop() override = default;
+
+        /// @brief Returns true if another instance is already running. In that
+        /// case the new instance will also send its args to the primary.
+        bool isRunning();
+
+    signals:
+        /// @brief Emitted in the primary instance when a secondary instance
+        /// connects. The provided args are those received from the new
+        /// instance.
+        void appRelaunched(QStringList args);
+
+    private:
+        QStringList args_;
+        QString key_;
+
+        QLocalServer* server_ = nullptr;
+        QTimer* debouncer_ = new QTimer(this);
+
+        bool serverExists_() const;
+        void sendArgs_(QLocalSocket& socket) const;
+        void startServer_();
+
+    private slots:
+        void onServerNewConnection_();
+
+    }; // class Coco::StartCop
+
+} // namespace Coco
+
+#else // #if !defined(QT_NETWORK_LIB) && !defined(QT_QTNETWORK_MODULE_H)
+
+#warning "Qt Network module not available. Coco::StartCop class will be disabled."
+
+#endif // #if defined(QT_NETWORK_LIB) || defined(QT_QTNETWORK_MODULE_H)
---
+++ Coco/src/StartCop.cpp
@@ -0,0 +1,133 @@
+#if defined(QT_NETWORK_LIB) || defined(QT_QTNETWORK_MODULE_H)
+
+#include <QByteArray>
+#include <QLocalServer>
+#include <QLocalSocket>
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QTimer>
+
+#include "../include/Coco/StartCop.h"
+
+// ----- Internal -----
+
+constexpr auto WAIT = 100;
+constexpr auto TIMEOUT = 1000;
+constexpr auto DELIMITER = '\0';
+
+static QByteArray serialize_(const QStringList& args)
+{
+    return args.join(DELIMITER).toUtf8();
+}
+
+static QStringList deserialize_(const QByteArray& data)
+{
+    return QString::fromUtf8(data).split(DELIMITER);
+}
+
+static QStringList toQStringList_(const int& argc, const char* const* argv)
+{
+    QStringList args{};
+
+    for (auto i = 0; i < argc; ++i)
+        args << QString::fromUtf8(argv[i]);
+
+    return args;
+}
+
+// ----- End Internal -----
+
+namespace Coco
+{
+    StartCop::StartCop
+    (
+        const QString& key,
+        const int& argc,
+        const char* const* argv
+    )
+        : QObject(nullptr)
+        , args_(toQStringList_(argc, argv))
+        , key_(key)
+    {
+        debouncer_->setSingleShot(true);
+    }
+
+    bool StartCop::isRunning()
+    {
+        if (serverExists_())
+            return true;
+
+        startServer_();
+        return false;
+    }
+
+    bool StartCop::serverExists_() const
+    {
+        QLocalSocket socket{};
+        socket.connectToServer(key_);
+        auto exists = socket.waitForConnected(WAIT);
+
+        if (exists)
+            sendArgs_(socket);
+
+        socket.close();
+        return exists;
+    }
+
+    void StartCop::sendArgs_(QLocalSocket& socket) const
+    {
+        auto data = serialize_(args_);
+        socket.write(data);
+        socket.flush();
+        socket.waitForBytesWritten(WAIT);
+    }
+
+    void StartCop::startServer_()
+    {
+        server_ = new QLocalServer(this);
+        server_->setSocketOptions(QLocalServer::WorldAccessOption);
+
+        // Try to listen; if this fails (perhaps due to a stale socket), remove
+        // it and try again. On Unix-like systems, if the application crashes or
+        // terminates unexpectedly, the local socket file may not be removed.
+        // This can cause subsequent calls to listen(key_) to fail.
+        if (!server_->listen(key_))
+        {
+            QLocalServer::removeServer(key_);
+            server_->listen(key_);
+        }
+
+        connect
+        (
+            server_,
+            &QLocalServer::newConnection,
+            this,
+            &StartCop::onServerNewConnection_
+        );
+    }
+
+    void StartCop::onServerNewConnection_()
+    {
+        if (debouncer_->isActive()) return;
+
+        auto next = server_->nextPendingConnection();
+        if (!next) return;
+
+        if (next->waitForReadyRead(WAIT))
+        {
+            auto data = next->readAll();
+            auto new_args = deserialize_(data);
+
+            emit appRelaunched(new_args);
+        }
+
+        next->disconnectFromServer();
+        next->deleteLater();
+
+        debouncer_->start(TIMEOUT);
+    }
+
+} // namespace Coco
+
+#endif // #if defined(QT_NETWORK_LIB) || defined(QT_QTNETWORK_MODULE_H)

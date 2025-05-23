9d75c609d527cb5000413225ac2e328113c97f69
jackson <63016612+fairybow@users.noreply.github.com>
2025-05-23 02:35:33 -0400

    Update Debug.h

--- Coco/include/Coco/Debug.h
+++ Coco/include/Coco/Debug.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <QDebug>
+#include <QTimer>
 
 // Can be replaced later with more complex (and optional & toggleable) logging
 #define COCO_TRACER qDebug() << __FUNCTION__
+
+#define COCO_TEST_TIMER(Interval, Receiver, Slot) \
+    QTimer::singleShot(Interval, Receiver, Slot);
+
+#define COCO_TEST_TIMER(Interval, Slot) \
+    QTimer::singleShot(Interval, this, Slot);

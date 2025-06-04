7226867495af1bea7e5a99ae84b4e232aec05c2a
jackson <63016612+fairybow@users.noreply.github.com>
2025-06-04 11:56:37 -0400

    Update Debug.h

--- Coco/include/Coco/Debug.h
+++ Coco/include/Coco/Debug.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <QApplication>
 #include <QDebug>
+#include <QObject>
 #include <QTimer>
 
 // Can be replaced later with more complex (and optional & toggleable) logging.
@@ -10,24 +12,16 @@
 #define COCO_QSST(Interval, Slot) QTimer::singleShot(Interval, this, Slot)
 
 // Convenience macro for quick tests. Not for regular use.
-#define COCO_QREPEATER(interval, repeats, slot)                             \
-    do {                                                                    \
-        static QTimer* timer = nullptr;                                     \
-        static int count = 0;                                               \
-        if (!timer)                                                         \
-        {                                                                   \
-            timer = new QTimer(QCoreApplication::instance());               \
-            QObject::connect(timer, &QTimer::timeout, slot);                \
-            QObject::connect(timer, &QTimer::timeout, [&] {                 \
-                count++;                                                    \
-                if (count < repeats) return;                                \
-                timer->deleteLater();                                       \
-                timer = nullptr;                                            \
-                count = 0;                                                  \
-            });                                                             \
-                                                                            \
-            timer->start(interval);                                         \
-        }                                                                   \
+#define COCO_QREPEATER(interval, repeats, slot)             \
+    do {                                                    \
+        auto timer = new QTimer(QApplication::instance());  \
+        QObject::connect(timer, &QTimer::timeout, slot);    \
+        QObject::connect(timer, &QTimer::timeout, [timer] { \
+            static auto counter = 0;                        \
+            counter++;                                      \
+            if (counter >= repeats) timer->deleteLater();   \
+        });                                                 \
+        timer->start(interval);                             \
     } while(0)
 
 // Maybe conditional tracing?

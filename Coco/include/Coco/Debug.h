a647686565999396a5c3c4fc2947e3276a2435f2
jackson <63016612+fairybow@users.noreply.github.com>
2025-06-03 20:11:16 -0400

    Update Debug.h

--- Coco/include/Coco/Debug.h
+++ Coco/include/Coco/Debug.h
@@ -29,3 +29,25 @@
             timer->start(interval);                                         \
         }                                                                   \
     } while(0)
+
+// Maybe conditional tracing?
+//#ifdef DEBUG
+//#define COCO_DESTRUCTOR_TRACE { COCO_TRACER; }
+//#else
+//#define COCO_DESTRUCTOR_TRACE {}
+//#endif
+// Using ~Class() override COCO_DESTRUCTOR_TRACE in the below:
+
+/// @brief I occasionally subclass in order to: 1) check memory via destructor
+/// debug output; and 2) have debug output  show up as a custom class and not,
+/// say, QWidget or something unhelpful. Obviously, this doesn't work directly
+/// as a nested class nor with `std::unique_ptr`.
+#define COCO_TRIVIAL_QCLASS(Class, QtObject)        \
+    class Class : public QtObject                   \
+    {                                               \
+        Q_OBJECT                                    \
+                                                    \
+    public:                                         \
+        using QtObject::QtObject;                   \
+        virtual ~Class() override { COCO_TRACER; }  \
+    }

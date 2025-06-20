8ab344a9ca4220cdb70123a5c13c2b9d7fe5b28c
jackson <63016612+fairybow@users.noreply.github.com>
2025-06-19 23:23:42 -0400

    Add COCO_LOG_THIS macro with vocative prefix
    Introduced the COCO_LOG_THIS macro to log messages with a prefix based on the current object's string representation using COCO_PTR_QSTR(this). This aids in identifying the source object in log outputs.

--- Coco/include/Coco/Log.h
+++ Coco/include/Coco/Log.h
@@ -3,5 +3,10 @@
 #include <QDebug>
 #include <QString>
 
+#include "Debug.h"
+
 // Temporary implementation!
 #define COCO_LOG(Message) qDebug() << qUtf8Printable(Message)
+
+// Temporary implementation! Adds a "vocative" prefix
+#define COCO_LOG_THIS(Message) qDebug() << qUtf8Printable(QString("%0: %1").arg(COCO_PTR_QSTR(this)).arg(Message))

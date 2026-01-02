ba10011192a75d2ac0f5802765f9d4dc2d336e00
jackson <63016612+fairybow@users.noreply.github.com>
2026-01-02 00:01:50 -0500

    Refactor StartCop class and improve formatting

--- Coco/include/Coco/StartCop.h
+++ Coco/include/Coco/StartCop.h
@@ -2,79 +2,76 @@
 
 #if defined(QT_NO_NETWORK)
 
-#warning "Qt Network module not available. Coco::StartCop class will be disabled."
+#    warning                                                                   \
+        "Qt Network module not available. Coco::StartCop class will be disabled."
 
-#else // #if !defined(QT_NO_NETWORK)
+#else
 
-#include <QLocalServer>
-#include <QLocalSocket>
-#include <QObject>
-#include <QString>
-#include <QStringList>
-#include <QTimer>
+#    include <QLocalServer>
+#    include <QLocalSocket>
+#    include <QObject>
+#    include <QString>
+#    include <QStringList>
+#    include <QTimer>
 
-namespace Coco
+namespace Coco {
+
+// Coco::StartCop is a QObject-derived utility designed to manage
+// single-instance applications by detecting if another instance is running and
+// stopping it after sending the second instance's command line arguments to the
+// primary instance.
+//
+// Example:
+//
+// ```
+// #include "Coco/StartCop.h"
+//
+// int main(int argc, char* argv[])
+// {
+//     Coco::StartCop s("My App", argc, argv);
+//     if (s.isRunning()) return 0;
+//
+//     QApplication app(argc, argv);
+//
+//     MainWindow w;
+//     w.connect(&s, &StartCop::appRelaunched, &w, &MainWindow::slot);
+//     w.show();
+//
+//     return app.exec();
+// }
+// ```
+class StartCop : public QObject
 {
-    /// @brief Coco::StartCop is a QObject-derived utility designed to manage
-    /// single-instance applications by detecting if another instance is running
-    /// and stopping it after sending the second instance's command line
-    /// arguments to the primary instance.
-    ///
-    /// @example
-    ///     #include "Coco/StartCop.h"
-    ///
-    ///     int main(int argc, char* argv[])
-    ///     {
-    ///         Coco::StartCop s("My App", argc, argv);
-    ///         if (s.isRunning()) return 0;
-    ///
-    ///         QApplication app(argc, argv);
-    ///
-    ///         MainWindow w;
-    ///         w.connect(&s, &StartCop::appRelaunched,
-    ///             &w, &MainWindow::slot);
-    ///         w.show();
-    ///
-    ///         return app.exec();
-    ///     }
-    class StartCop : public QObject
-    {
-        Q_OBJECT
+    Q_OBJECT
 
-    public:
-        StartCop(
-            const QString &key,
-            const int &argc = 0,
-            const char *const *argv = nullptr);
+public:
+    StartCop(
+        const QString& key,
+        const int& argc = 0,
+        const char* const* argv = nullptr);
 
-        virtual ~StartCop() override = default;
+    virtual ~StartCop() override = default;
 
-        /// @brief Returns true if another instance is already running. In that
-        /// case the new instance will also send its args to the primary.
-        bool isRunning();
+    bool isRunning();
 
-    signals:
-        /// @brief Emitted in the primary instance when a secondary instance
-        /// connects. The provided args are those received from the new
-        /// instance.
-        void appRelaunched(const QStringList& args);
+signals:
+    void appRelaunched(const QStringList& args);
 
-    private:
-        QStringList args_;
-        QString key_;
+private:
+    QStringList args_;
+    QString key_;
 
-        QLocalServer *server_ = nullptr;
-        QTimer *debouncer_ = new QTimer(this);
+    QLocalServer* server_ = nullptr;
+    QTimer* debouncer_ = new QTimer(this);
 
-        bool serverExists_() const;
-        void sendArgs_(QLocalSocket &socket) const;
-        void startServer_();
+    bool serverExists_() const;
+    void sendArgs_(QLocalSocket& socket) const;
+    void startServer_();
 
-    private slots:
-        void onServerNewConnection_();
-
-    }; // class Coco::StartCop
+private slots:
+    void onServerNewConnection_();
+};
 
 } // namespace Coco
 
-#endif // #if defined(QT_NO_NETWORK)
+#endif

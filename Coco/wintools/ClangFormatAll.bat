70f85f7d8a92dab14a604adbd7f3fafcf4ad6f76
jackson <63016612+fairybow@users.noreply.github.com>
2026-08-01 15:11:09 -0400

    Update ClangFormatAll.bat

--- Coco/wintools/ClangFormatAll.bat
+++ Coco/wintools/ClangFormatAll.bat
@@ -1,6 +1,10 @@
 @echo off
 set CF="C:\Program Files\Microsoft Visual Studio\18\Community\VC\Tools\Llvm\bin\clang-format.exe"
 
+for /r "%~dp0..\include\Coco" %%f in (*.h *.mm) do (
+    %CF% -i "%%f"
+)
+
 for /r "%~dp0..\src" %%f in (*.h *.cpp *.mm) do (
     %CF% -i "%%f"
 )

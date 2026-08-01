8d1425d4bdf06c59f85688194c094e112ea88779
jackson <63016612+fairybow@users.noreply.github.com>
2026-07-31 20:13:05 -0400

    Adding SrcCopy.bat

---
+++ Coco/wintools/SrcCopy.bat
@@ -0,0 +1,48 @@
+@echo off
+setlocal
+
+REM ----------------------------------------
+
+set ROOT_DIR=%~dp0..\..
+set PROJ_DIR=%~dp0..
+set INCLUDE_DIR=%~dp0..\include\Coco
+set SRC_DIR=%~dp0..\src
+set DOCS_DIR=%~dp0..\docs
+
+set OUT_DIR=%~dp0..\..\..\CocoSrc
+
+REM ----------------------------------------
+
+if exist "%OUT_DIR%" rmdir /s /q "%OUT_DIR%"
+mkdir "%OUT_DIR%"
+
+copy "%ROOT_DIR%\README.md" "%OUT_DIR%\" > nul
+
+copy "%PROJ_DIR%\.clang-format" "%OUT_DIR%\" > nul
+REM copy "%PROJ_DIR%\CMakeLists.txt" "%OUT_DIR%\" > nul
+REM copy "%PROJ_DIR%\cpp.hint" "%OUT_DIR%\" > nul
+
+for /r "%INCLUDE_DIR%" %%f in (*) do (
+    copy "%%f" "%OUT_DIR%\" > nul
+)
+
+for /r "%SRC_DIR%" %%f in (*) do (
+    copy "%%f" "%OUT_DIR%\" > nul
+)
+
+for /r "%DOCS_DIR%" %%f in (*) do (
+    copy "%%f" "%OUT_DIR%\" > nul
+)
+
+REM ----------------------------------------
+
+pushd "%ROOT_DIR%"
+tree /f /a > "%OUT_DIR%\_tree.txt"
+popd
+
+pushd "%OUT_DIR%"
+echo Copied to: %CD%
+popd
+
+pause
+endlocal
